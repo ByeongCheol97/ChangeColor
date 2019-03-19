14e5f5d73b0e53993e7c5a441b24c4956fedb9f0
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-03-19 15:35:26 +0900

    commit

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var Change = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func Press(_ sender: Any) {
+        label.text = "Change Color 클릭시 배경색이 바뀝니다"
+    }
+    
+    @IBAction func Init(_ sender: Any) {
+        label.text = ""
+        self.view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func Change(_ sender: Any) {
+        if Change == true {
+                self.view.backgroundColor = UIColor.yellow
+            Change = false
+        } else {
+                self.view.backgroundColor = UIColor.white
+            Change = true
+        }
+    }
 }
 
